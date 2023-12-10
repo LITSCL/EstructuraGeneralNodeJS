7d09c748f08595501dc7e66ebe8ab7c116ffe8f7
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 19:50:18 -0300

    Ok

--- server/SQL-Clear/dbestructurageneralnodejs.sql
+++ server/SQL-Clear/dbestructurageneralnodejs.sql
@@ -1,55 +1,112 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-12-2023 a las 23:46:03
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.0
 
-CREATE SCHEMA IF NOT EXISTS `dbestructurageneralnodejs` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `dbestructurageneralnodejs` ;
-
--- -----------------------------------------------------
--- Table `dbestructurageneralnodejs`.`usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbestructurageneralnodejs`.`usuario` (
-  `rut` VARCHAR(45) NOT NULL ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `apellido` VARCHAR(45) NOT NULL ,
-  `email` VARCHAR(45) NOT NULL ,
-  `clave` VARCHAR(255) NOT NULL ,
-  `tipo` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`rut`) )
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `dbestructurageneralnodejs`.`categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbestructurageneralnodejs`.`categoria` (
-  `id` INT AUTO_INCREMENT NOT NULL ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC) )
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `dbestructurageneralnodejs`
+--
+CREATE DATABASE IF NOT EXISTS `dbestructurageneralnodejs` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dbestructurageneralnodejs`;
 
--- -----------------------------------------------------
--- Table `dbestructurageneralnodejs`.`producto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbestructurageneralnodejs`.`producto` (
-  `codigo` VARCHAR(45) NOT NULL ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `precio` DOUBLE NOT NULL ,
-  `categoria_id` INT NOT NULL ,
-  PRIMARY KEY (`codigo`, `categoria_id`) ,
-  INDEX `fk_producto_categoria_idx` (`categoria_id` ASC) ,
-  CONSTRAINT `fk_producto_categoria`
-    FOREIGN KEY (`categoria_id` )
-    REFERENCES `dbestructurageneralnodejs`.`categoria` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
-USE `dbestructurageneralnodejs` ;
+--
+-- Estructura de tabla para la tabla `categoria`
+--
 
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codigo` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `precio` double NOT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `rut` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `tipo` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codigo`,`categoria_id`),
+  ADD KEY `fk_producto_categoria_idx` (`categoria_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`rut`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
